8bacf2ed634639eee423373f8fe2ddf0ded36cfa
Matheus Bitencourt <matheus_si_kta@outlook.com>
2023-03-10 09:14:53 -0300

    Criando classe tabuleiro

--- test/modelo/CampoTeste.java
+++ test/modelo/CampoTeste.java
@@ -1,25 +1,91 @@
 package modelo;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import excecao.ExplosaoException;
 
 
 public class CampoTeste {
 	
-	private Campo campo = new Campo(3, 3);
+	private Campo campo;
 	
+	@BeforeEach
 	void iniciarCampo() {
-		
+		campo = new Campo(3,3);
 	}
 	
 	@Test
 	void testeVisinhoReal() {
 		Campo vizinho = new Campo(3,2);
-		
-		boolean resultadi = campo.adicionarVizinho(vizinho);
-		
-		assertTrue(resultadi);
+		boolean resultado = campo.adicionarVizinho(vizinho);	
+		assertTrue(resultado);
 	}
+	
+	@Test
+	void testeVisinhoDistancia1Embaixo() {
+		Campo vizinho = new Campo(4,3);
+		boolean resultado = campo.adicionarVizinho(vizinho);	
+		assertTrue(resultado);
+	}
+	
+	@Test
+	void testeVisinhoDistancia2() {
+		Campo vizinho = new Campo(2,2);
+		boolean resultado = campo.adicionarVizinho(vizinho);	
+		assertTrue(resultado);
+	}
+	
+	@Test
+	void testeNaoVisinho() {
+		Campo vizinho = new Campo(1,1);
+		boolean resultado = campo.adicionarVizinho(vizinho);	
+		assertFalse(resultado);
+	}
+	
+	@Test
+	void testeValorPadraoAtributoMarcado() {		
+		assertFalse(campo.isMarcado());
+	}
+	
+	@Test
+	void testeAlternarMarcacao() {
+		campo.alternarMarcacao();
+		assertTrue(campo.isMarcado());
+	}
+	
+	@Test
+	void testeAlternarMarcacaoDuasChamadas() {
+		campo.alternarMarcacao();
+		campo.alternarMarcacao();
+		assertFalse(campo.isMarcado());
+	}
+	
+	@Test
+	void testeAbrirNaoMinadoMarcado() {
+		campo.alternarMarcacao();
+		assertFalse(campo.abrir());
+	}
+	
+	@Test
+	void testeAbrirMinadoMarcado() {
+		campo.alternarMarcacao();
+		campo.minar();
+		assertFalse(campo.abrir());
+	}
+	
+	@Test
+	void testeAbrirMinadoNaoMarcado() {
+		campo.minar();
+		
+		assertThrows(ExplosaoException.class,() ->{
+		campo.abrir();
+		});
+	}
+	
 	
 }
